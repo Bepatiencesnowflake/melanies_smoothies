1f449023816f8068f3877d72c77913f0edddd356
Bepatiencesnowflake <168467747+Bepatiencesnowflake@users.noreply.github.com>
2024-04-29 20:07:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ if ingredients_list:
     for  furit_choosen in ingredients_list:
         ingredients_string +=   furit_choosen + '    '     
         #st.subheader(fruit_choosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width= True)
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choosen)
+        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width= True)
+        st.write(fruit_choosen)
         
     sqlstatement = """ insert into smoothies.public.orders(ingredients)
                 values ('""" + ingredients_string + """')""";
