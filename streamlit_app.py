0a3bd96f696a7426b783fd50d96e36c442e593cc
Bepatiencesnowflake <168467747+Bepatiencesnowflake@users.noreply.github.com>
2024-04-29 21:49:41 -0400

    Update streamlit_app.py
    adding a new search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,13 @@ from snowflake.snowpark.functions import col
 cnx = st.connection ("snowflake")
 session = cnx.session()
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session. table("smoothies. public. fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON '))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st. stop()
+
+
+
 
 ingredients_list = st.multiselect(
     'Choose Upto 5 Ingredients:'
